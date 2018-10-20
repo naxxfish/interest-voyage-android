06d6dd53cacd145d5cfc03e9aaf4bc47773d9ece
Chris Roberts <1645367+naxxfish@users.noreply.github.com>
2018-10-20 10:58:08 +0100

    Added station picker activity

--- app/src/main/java/net/naxxfish/interestvoyage/MainActivity.java
+++ app/src/main/java/net/naxxfish/interestvoyage/MainActivity.java
@@ -1,7 +1,10 @@
 package net.naxxfish.interestvoyage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClickFindTrains(View v) {
+       TextView statusTextView = findViewById(R.id.statusTextView);
+       statusTextView.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickStartingStation(View v) {
+        Intent intent = new Intent(this, StationPicker.class);
+        startActivity(intent);
     }
 
 }
